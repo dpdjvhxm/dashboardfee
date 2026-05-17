4538dae38a4164cb1aef9439af7f519c87d3b7d1
Jaehoon <137974842+dpdjvhxm@users.noreply.github.com>
2026-05-17 17:52:44 +0900

    Load Coupang logo stylesheet

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './styles.css';
+import './logo.css';
 import { initI18n } from './i18n.js';
 
 createRoot(document.getElementById('root')).render(<App />);
